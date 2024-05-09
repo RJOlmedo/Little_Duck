c9b3414d39c8d19fde84c80a96ebc9248c2599e0
Ricardo Olmedo <rjolmedoc@gmail.com>
2024-05-09 16:53:07 -0600

    Correct Pars example and grammar

--- little_duck_pars.py
+++ little_duck_pars.py
@@ -1,185 +1,149 @@
-import ply.yacc as yacc
+from ply import yacc
 from little_duck_lex import tokens
 
-# Precedencia de operadores
-precedence = (
-    ('left', 'PLUS', 'MINUS'),
-    ('left', 'MUL', 'DIV'),
-    ('nonassoc', 'LT', 'GT', 'EQ', 'NE'),
-)
-
-# Gramática y reglas de producción
-def p_program(p):
-    'program : PROGRAM ID SEMI vars funcs main END'
-    p[0] = ('program', p[2], p[4], p[5], p[6])
+def p_programa(p):
+    'Programa : PROGRAM ID SEMICOLON VARS FUNCS MAIN Body END SEMICOLON'
+    pass
 
 def p_vars(p):
-    '''vars : VAR var_declaration vars
-            | empty'''
-    if len(p) == 4:
-        p[0] = ('vars', p[2], p[3])
-    else:
-        p[0] = None
+    '''VARS : VAR ListaVars COLON TYPE SEMICOLON
+            | epsilon'''
+    pass
 
-def p_var_declaration(p):
-    '''var_declaration : ID COLON type SEMI
-                       | ID COLON type COMMA var_declaration'''
-    if len(p) == 5:
-        p[0] = ('var', p[1], p[3])
-    else:
-        p[0] = ('var', p[1], p[3], p[5])
+def p_lista_vars(p):
+    '''ListaVars : ID COMMA ListaVars
+                 | ID'''
+    pass
 
 def p_type(p):
-    '''type : INT
+    '''TYPE : INT
             | FLOAT
             | STRING'''
-    p[0] = p[1]
+    pass
 
 def p_funcs(p):
-    '''funcs : VOID ID LPAREN parameter_list RPAREN LBRACE statement_list RBRACE funcs
-             | empty'''
-    if len(p) == 10:
-        p[0] = ('funcs', p[2], p[4], p[7], p[9])
-    else:
-        p[0] = None
+    '''FUNCS : Funcion FUNCS
+             | epsilon'''
+    pass
 
-def p_parameter_list(p):
-    '''parameter_list : parameter COMMA parameter_list
-                      | parameter
-                      | empty'''
-    if len(p) == 4:
-        p[0] = [p[1]] + p[3]
-    elif len(p) == 2:
-        p[0] = [p[1]]
-    else:
-        p[0] = []
+def p_funcion(p):
+    'Funcion : TipoFunc ID LPAREN Parametros RPAREN COLON Body'
+    pass
 
-def p_parameter(p):
-    'parameter : ID COLON type'
-    p[0] = ('param', p[1], p[3])
+def p_tipofunc(p):
+    '''TipoFunc : VOID
+                | TYPE'''
+    pass
 
-def p_main(p):
-    'main : MAIN LBRACE statement_list RBRACE'
-    p[0] = ('main', p[3])
+def p_parametros(p):
+    '''Parametros : ID COMMA Parametros
+                  | ID COLON TYPE
+                  | epsilon'''
+    pass
 
-def p_statement_list(p):
-    '''statement_list : statement_list statement
-                      | statement'''
-    if len(p) == 3:
-        p[0] = p[1] + [p[2]]
-    else:
-        p[0] = [p[1]]
+def p_body(p):
+    'Body : LBRACE Statements RBRACE'
+    pass
+
+def p_statements(p):
+    '''Statements : Statement Statements
+                  | epsilon'''
+    pass
 
 def p_statement(p):
-    '''statement : assign
-                 | condition
-                 | cycle
-                 | print_statement
-                 | f_call'''
-    p[0] = p[1]
+    '''Statement : ASSIGNMENT
+                 | CONDITION
+                 | CYCLE
+                 | F_Call
+                 | Print'''
+    pass
 
-def p_assign(p):
-    'assign : ID ASSIGN expression SEMI'
-    p[0] = ('assign', p[1], p[3])
+def p_assignment(p):
+    'ASSIGNMENT : ID ASSIGN Expresion SEMICOLON'
+    pass
 
 def p_condition(p):
-    'condition : IF LPAREN expression RPAREN LBRACE statement_list RBRACE else_part'
-    p[0] = ('condition', p[3], p[6], p[8])
-
-def p_else_part(p):
-    '''else_part : ELSE LBRACE statement_list RBRACE
-                 | empty'''
-    if len(p) == 5:
-        p[0] = p[3]
-    else:
-        p[0] = None
+    '''CONDITION : IF LPAREN Expresion RPAREN Body
+                 | IF LPAREN Expresion RPAREN Body ELSE Body'''
+    pass
 
 def p_cycle(p):
-    '''cycle : WHILE LPAREN expression RPAREN LBRACE statement_list RBRACE
-             | DO LBRACE statement_list RBRACE WHILE LPAREN expression RPAREN SEMI'''
-    if p[1] == 'while':
-        p[0] = ('while', p[3], p[6])
-    else:
-        p[0] = ('do_while', p[3], p[7])
-
-def p_print_statement(p):
-    'print_statement : PRINT LPAREN expression RPAREN SEMI'
-    p[0] = ('print', p[3])
+    '''CYCLE : WHILE LPAREN Expresion RPAREN Body SEMICOLON
+             | DO Body WHILE LPAREN Expresion RPAREN SEMICOLON'''
+    pass
 
 def p_f_call(p):
-    '''f_call : ID LPAREN arg_list RPAREN SEMI
-              | ID LPAREN RPAREN SEMI'''
-    if len(p) == 6:
-        p[0] = ('f_call', p[1], p[3])
-    else:
-        p[0] = ('f_call', p[1], [])
+    'F_Call : ID LPAREN Expresiones RPAREN SEMICOLON'
+    pass
 
-def p_arg_list(p):
-    '''arg_list : expression COMMA arg_list
-                | expression'''
-    if len(p) == 4:
-        p[0] = [p[1]] + p[3]
-    else:
-        p[0] = [p[1]]
+def p_expresiones(p):
+    '''Expresiones : Expresion COMMA Expresiones
+                   | Expresion
+                   | epsilon'''
+    pass
 
-def p_expression(p):
-    '''expression : expression PLUS expression
-                  | expression MINUS expression
-                  | expression MUL expression
-                  | expression DIV expression
-                  | term'''
-    if len(p) == 4:
-        p[0] = ('binary_op', p[2], p[1], p[3])
-    else:
-        p[0] = p[1]
+def p_print(p):
+    'Print : PRINT LPAREN Expresiones RPAREN SEMICOLON'
+    pass
+
+def p_expresion(p):
+    '''Expresion : Expresion NOTEQUALS Exp
+                 | Expresion LESS Exp
+                 | Expresion GREATER Exp
+                 | Exp'''
+    pass
+
+def p_exp(p):
+    '''Exp : Exp PLUS Term
+           | Exp MINUS Term
+           | Term'''
+    pass
 
 def p_term(p):
-    '''term : factor
-            | factor LT factor
-            | factor GT factor
-            | factor EQ factor
-            | factor NE factor'''
-    if len(p) == 4:
-        p[0] = ('comparison_op', p[2], p[1], p[3])
-    else:
-        p[0] = p[1]
+    '''Term : Term TIMES Factor
+            | Term DIVIDE Factor
+            | Factor'''
+    pass
 
 def p_factor(p):
-    '''factor : LPAREN expression RPAREN
-              | ID
-              | CTE_INT
-              | CTE_FLOAT
-              | CTE_STRING'''
-    if len(p) == 4:
-        p[0] = p[2]
-    else:
-        p[0] = p[1]
+    '''Factor : LPAREN Expresion RPAREN
+              | PLUS Subfactor
+              | MINUS Subfactor
+              | Subfactor'''
+    pass
 
-def p_empty(p):
-    'empty :'
+def p_subfactor(p):
+    '''Subfactor : CTE
+                 | ID'''
+    pass
+
+def p_cte(p):
+    '''CTE : CTE_FLOAT
+           | CTE_INT
+           | CTE_STRING'''
+    pass
+
+def p_epsilon(p):
+    'epsilon :'
     pass
 
 def p_error(p):
     if p:
-        print(f"Error de sintaxis en token {p.type}, línea {p.lineno}, posición {p.lexpos}")
+        print(f"Syntax error '{p.value}' on line {p.lineno}")
     else:
-        print("Error de sintaxis en EOF")
+        print("Syntax error at EOF")
 
-# Construir el parser
-parser = yacc.yacc()
+parser = yacc.yacc(debug=True)
 
 # Prueba del parser
 if __name__ == '__main__':
     data = '''
     program ejemplo;
-    var x : int, y : float;
+    var x : int;
     main {
         print("Hola Mundo");
-        if (x < 10) {
-            print("Hola Mundo");
-        }
     }
-    end
+    end;
     '''
     result = parser.parse(data)
     print(result)
